5308ac2455ff188f9492240f89d37831bb4f1d84
Sai Vishnu Edala <saivishnu.edala@gmail.com>
2023-05-31 13:38:49 -0500

    validating string with zero

---
+++ string.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# set -x
+STR1=$1
+STR2=$2
+if [ $# -lt 2 ]; then
+    echo "You are suppose to give two arguments"
+else
+    if [ -z $STR1 -o -z $STR2 ]; then
+        echo "One of the param is zero"
+    elif [ $STR1 = 'ZERO' -o $STR2 = 'ZERO' ]; then
+        echo "You have given both zero"
+    else
+        if [ "${STR1}" = "${STR2}" ]; then
+            echo "Both are same"
+        else
+            echo "Both are diff"
+        fi
+    fi
+fi
+
+
+
